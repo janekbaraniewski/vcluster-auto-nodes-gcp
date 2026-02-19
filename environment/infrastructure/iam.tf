ec0ccbc142610860774ce0c5b3434b8cfaa7d683
Jan Baraniewski <dev@baraniewski.com>
2026-02-19 10:43:26 +0100

    Update security role

--- environment/infrastructure/iam.tf
+++ environment/infrastructure/iam.tf
@@ -14,36 +14,12 @@ resource "google_project_iam_member" "ccm_roles" {
     "roles/compute.viewer",
     "roles/compute.loadBalancerAdmin",
     "roles/compute.instanceAdmin.v1",
+    "roles/compute.securityAdmin",
     "roles/iam.serviceAccountUser",
   ]) : toset([])
 
   project = local.project
   role    = each.value
-  member  = "serviceAccount:${google_service_account.vcluster_node.email}"
-}
-
-resource "google_project_iam_custom_role" "ccm_firewall_min" {
-  for_each = local.ccm_enabled ? { enabled = true } : {}
-
-  project     = local.project
-  role_id     = replace(format("ccm-firewall-%s", local.random_id), "-", "_")
-  title       = format("CCM firewall for %s", local.vcluster_name)
-  description = format("Minimal VPC firewall permissions for CCM, used by %s", local.vcluster_name)
-  permissions = [
-    "compute.firewalls.create",
-    "compute.firewalls.delete",
-    "compute.firewalls.get",
-    "compute.firewalls.list",
-    "compute.firewalls.update",
-    "compute.networks.updatePolicy",
-  ]
-}
-
-resource "google_project_iam_member" "ccm_firewall_binding" {
-  for_each = local.ccm_enabled ? { enabled = true } : {}
-
-  project = local.project
-  role    = google_project_iam_custom_role.ccm_firewall_min[each.key].name
   member  = "serviceAccount:${google_service_account.vcluster_node.email}"
 }
 
